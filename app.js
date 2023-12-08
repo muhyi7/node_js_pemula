66ca784ef18dfc5c99e4bad8c09c38d88b00d302
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-12-09 04:49:36 +0700

    module npm library

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.copyFileSync("text.txt", "text2.txt");
-console.log("text.txt was copied to text2.txt")
+// fs.copyFileSync("text.txt", "text2.txt");
+// console.log("text.txt was copied to text2.txt")
+
+const hero = require("superheroes")
+
+for(let i = 0; i < 10; i++){
+    let nameHero = hero.random();
+    console.log(hero.random());
+}
