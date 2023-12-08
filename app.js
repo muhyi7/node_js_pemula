fe6e734a75af3c4b46139f75f7ee8397f2e38ccb
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-12-09 05:06:11 +0700

    custom module

--- app.js
+++ app.js
@@ -3,9 +3,19 @@
 // fs.copyFileSync("text.txt", "text2.txt");
 // console.log("text.txt was copied to text2.txt")
 
-const hero = require("superheroes")
+// const hero = require("superheroes")
 
-for(let i = 0; i < 10; i++){
-    let nameHero = hero.random();
-    console.log(hero.random());
-}
+// for(let i = 0; i < 10; i++){
+//     let nameHero = hero.random();
+//     console.log(hero.random());
+// }
+
+const op  = require("./module");
+
+const moduleTitle = op.title;
+const moduleTambah = op.tambah(10,5);
+const modulePerkalian = op.kali(10,5);
+
+console.log(moduleTitle);
+console.log(moduleTambah);
+console.log(modulePerkalian);
